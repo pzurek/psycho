f305fd83d88349f49c36ddb0936aceb0fac83c6c
p.zurek <p.zurek@6f2d7086-9719-0410-ba06-0b8292a297aa>
2007-03-31 11:48:09 +0000

    Email in the license
    git-svn-id: https://psycho.googlecode.com/svn/trunk@122 6f2d7086-9719-0410-ba06-0b8292a297aa

--- psycho/Psycho/Core/TopicStyle.cs
+++ psycho/Psycho/Core/TopicStyle.cs
@@ -1,7 +1,7 @@
 // Copyright (C) 2006 by:
 //
 // Author:
-//   Piotr Zurek
+//   Piotr Zurek, p.zurek@gmail.com
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
