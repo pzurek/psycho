29c6460c1c42bd6d20990d5c638459ae3475f1ca
p.zurek <p.zurek@6f2d7086-9719-0410-ba06-0b8292a297aa>
2007-03-31 10:14:58 +0000

    Just playing with SVN again.
    git-svn-id: https://psycho.googlecode.com/svn/trunk@41 6f2d7086-9719-0410-ba06-0b8292a297aa

---
+++ psycho_0.0.1/Psycho/GUI/PsychoNodeView.cs
@@ -0,0 +1,86 @@
+// Copyright (C) 2006 by:
+//
+// Author:
+//   Piotr Zurek
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using Gtk;
+
+namespace Psycho {
+
+    public partial class PsychoNodeView : NodeView {
+
+        private PsychoTreeNode centralNode = new PsychoTreeNode("", "");
+        private PsychoTreeNode selectedNode = new PsychoTreeNode("", "");
+
+        public PsychoNodeView() : base() {
+
+            AppendColumn("Title", new CellRendererText(), "text", 0);
+            AppendColumn("GUID", new CellRendererText(), "text", 1);
+
+            Selection.Changed += new System.EventHandler(OnSelectionChanged);
+            RowCollapsed += new RowCollapsedHandler(outlineView_RowCollapsed);
+            RowExpanded += new RowExpandedHandler(outlineView_RowExpanded);
+            KeyPressEvent += new KeyPressEventHandler(outlineView_KeyPressEvent);
+            ExpanderColumn.Expand = true;
+
+            NodeSelection.Mode = SelectionMode.Single;
+            ShowAll();
+        }
+
+        void outlineView_KeyPressEvent(object o, KeyPressEventArgs args)
+        {
+            string key = args.Event.Key.ToString();
+            Console.WriteLine(key);
+        }
+
+        void OnSelectionChanged(object sender, System.EventArgs args)
+        {
+            if (NodeSelection.SelectedNode != null) {
+                selectedNode = checked((Psycho.PsychoTreeNode) NodeSelection.SelectedNode);
+                Console.WriteLine("Selection changed in the view: " + selectedNode.GUID);
+                SetCurrentTopic();
+            }
+        }
+
+        private void titleCell_Edited(object sender, Gtk.EditedArgs args)
+        {
+            EditTitle(args.NewText);
+        }
+
+        private void outlineView_RowCollapsed(object sender, Gtk.RowCollapsedArgs args)
+        {
+            TreePath path = args.Path;
+            PsychoTreeNode node = checked((Psycho.PsychoTreeNode) store.GetNode(path));
+            ExpandTopic(node.GUID, false);
+        }
+
+        private void outlineView_RowExpanded(object sender, Gtk.RowExpandedArgs args)
+        {
+            TreePath path = args.Path;
+            PsychoTreeNode node = checked((Psycho.PsychoTreeNode) store.GetNode(path));
+            ExpandTopic(node.GUID, true);
+        }
+    }
+}
