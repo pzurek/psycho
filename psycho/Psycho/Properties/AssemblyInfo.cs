4e0e10e1939afa90b30410b98cb41c93a4bbcab7
p.zurek <p.zurek@6f2d7086-9719-0410-ba06-0b8292a297aa>
2008-03-31 03:07:13 +0000

    Removing some ignores. Preparing to move the whole solution to Monodevelop.
    git-svn-id: https://psycho.googlecode.com/svn/trunk@310 6f2d7086-9719-0410-ba06-0b8292a297aa

---
+++ psycho/Psycho/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Psycho")]
+[assembly: AssemblyDescription("Free mind mapping solution")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Psycho Project")]
+[assembly: AssemblyProduct("Psycho")]
+[assembly: AssemblyCopyright("Copyright © Piotr Zurek 2007")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6484a792-2e43-40c7-8fca-f73d1adecfcf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
