62f53779da4f2ae4b67787c4217536992ab5a158
p.zurek <p.zurek@6f2d7086-9719-0410-ba06-0b8292a297aa>
2008-05-01 04:46:16 +0000

    * Some additional fields and properties
    * Guid replaced with ID which will be created as Guid.ToString and then it will always live as a string
    
    
    git-svn-id: https://psycho.googlecode.com/svn/trunk@358 6f2d7086-9719-0410-ba06-0b8292a297aa

---
+++ Psycho/src/Core/Psycho.Core/Psycho.Data/TopicList.cs
@@ -0,0 +1,61 @@
+//------10--------20--------30--------40--------50--------60--------70--------80
+//
+// TopicList.cs
+// 
+// Copyright (C) 2008 Piotr Zurek p.zurek@gmail.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.Collections.Generic;
+using Psycho.Core;
+
+namespace Psycho.Core
+{
+	public class TopicList : List<Topic>
+	{
+		Topic parent;
+		Topic first;
+		Topic last;
+
+		public TopicList (Topic my_topic) : base()
+		{
+			this.parent = my_topic;
+		}
+
+		public Topic Parent
+		{
+			get { return parent; }
+		}
+
+		public Topic First
+		{
+			get{
+				if (this.Count > 0)
+					return this[0];
+				return null;
+			}
+		}
+
+		public Topic Last
+		{
+			get{
+				if (this.Count > 0)
+					return this[this.Count];
+				return null;
+			}
+		}
+	}
+}
