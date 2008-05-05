ee9a16436cf4683f8160b3ec1607863d84275946
p.zurek <p.zurek@6f2d7086-9719-0410-ba06-0b8292a297aa>
2008-05-05 04:52:54 +0000

    Added MapItem class and IMapItem interface. All elements that need an ID should inherit from that class.
    git-svn-id: https://psycho.googlecode.com/svn/trunk@366 6f2d7086-9719-0410-ba06-0b8292a297aa

---
+++ Psycho/src/Core/Psycho.Core/Psycho.Data/MapItem.cs
@@ -0,0 +1,43 @@
+//------10--------20--------30--------40--------50--------60--------70--------80
+//
+// MapItem.cs
+// 
+// Copyright (C) 2008 Piotr Zurek p.zurek@gmail.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Psycho.Core
+{
+	public abstract class MapItem : IMapItem
+	{
+		private string id;
+
+		[XmlAttribute ()]
+		public string ID {
+			get {
+				if (id == null) {
+					Guid guid = System.Guid.NewGuid();
+					id = guid.ToString();
+				}
+				return id;
+			}
+			set { id = value; }
+		}
+	}
+}
